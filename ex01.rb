deee5d2129df436acb8517e0409a1f9cefa1d8ee
Karim Tarek <karimmtarek@gmail.com>
2014-08-08 02:04:47 -0400

    Initial commit

---
+++ ex01.rb
@@ -0,0 +1,7 @@
+# puts "Hello Karim!"
+# puts "Hello Again!"
+# puts "I like this!"
+# puts "This is fun Karim!"
+# puts "Yay! Printing"
+# puts "I'd much rather you 'not'!."
+puts "I 'said' do not touch this." + "\n" + "another line"
