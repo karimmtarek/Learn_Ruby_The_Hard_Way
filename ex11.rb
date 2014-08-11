34c1d3b0f2edb49917ce031f39d54f8f2b986d04
Karim Tarek <karimmtarek@gmail.com>
2014-08-11 04:47:07 -0400

    did some more exersises: 6, 7, 8, 9, 10, 11

---
+++ ex11.rb
@@ -0,0 +1,13 @@
+#################################
+# Exercise 11: Asking Questions #
+#################################
+
+print "How old are you? "
+age = gets.chomp()
+# puts age.to_s.length()
+print "How tall are you? "
+height = gets.chomp()
+print "Hom much do you weight? "
+weight= gets.chomp()
+
+puts "so you #{age} old, #{height} tall and #{weight} heavy."
